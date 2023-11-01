f86ce1dcad7d7146512e13fba0c437f3f08e68fb
0305940221 <ulabideenzain77@gmail.com>
2023-11-01 13:46:35 +0500

    firstcommit

---
+++ z.js
@@ -0,0 +1,93 @@
+// var num = "ali"
+// console.log(num);
+
+// let name1="zain"
+// console.log(name1)
+
+// const PI ="lahore"
+// console.log(PI);   
+
+// var name="zain"           
+// name="adil"
+// console.log(name)   adil
+
+// let name="zain"           
+// name="adil"
+// console.log(name)   adil
+
+// const name="zain"           
+// name="adil"
+// console.log(name)   in thi case error
+
+// function modjs(love){
+//     if (love){
+//      let name="zain"
+//      const sname="ul abideen"
+//     //  console.log("my name is "+ name +" "+ sname)
+//     }
+//     // console.log("my name is "+ name +" "+ sname)
+// }
+
+// modjs(true)
+
+// var whowillwintoday="india"     thiis line is globalscope
+// if (true){
+//      var whowillwintoday="pakistan";
+//      console.log(whowillwintoday)
+// }
+// console.log(whowillwintoday);
+
+
+// let whowillwintoday="india"     // globalscope
+// if (true){
+//      let whowillwintoday="pakistan";
+//      console.log(whowillwintoday)
+// }
+// console.log(whowillwintoday);
+
+// const whowillwintoday="india"     // globalscope
+// if (true){
+//      const whowillwintoday="pakistan";
+//      console.log(whowillwintoday)
+// }
+// console.log(whowillwintoday);
+
+// let obj={
+//     name:"zain"
+// }
+// let user=obj
+// user.name=  "adil"
+// console.log(obj)
+
+// let x="zain"
+// let y="x"
+// y="ahmed"
+// console.log(x)
+
+
+// let obj={
+//     name:"zain"
+// }
+// let user=obj     issue
+//  let user=Object.assign({},obj)    solution1
+// let user={...obj}     solution2
+// user.name=  "adil"
+// console.log(obj)
+// console.log(user)
+
+// let obj={
+//     name:"zain",
+//     address:{
+//         city:"okara", 
+//         state:"punjab",
+//         division : "sahiwal"
+//     }
+// }
+// let user=JSON.parse(JSON.stringify(obj))
+// user.address.city="Lahore"
+// // console.log(obj)
+// console.log(user)
+
+
+
+
